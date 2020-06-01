253e51229280267a5cf3cde3137dd55e68070402
Tom Roberts <tomroberts@lowrisc.org>
2020-06-01 10:30:31 +0100

    [rtl] Make speculative branch optional
    - The speculative branch behaviour causes a performance degradation of
      around 3% in the max config. This change enables that behaviour only
      the maximum PMP config, which is where it is most needed for timing
      closure.
    
    Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>

--- rtl/ibex_core.sv
+++ rtl/ibex_core.sv
@@ -111,6 +111,11 @@ module ibex_core #(
   localparam int unsigned PMP_NUM_CHAN      = 2;
   localparam bit          DataIndTiming     = SecureIbex;
   localparam bit          DummyInstructions = SecureIbex;
+  // Speculative branch option, trades-off performance against timing.
+  // Setting this to 1 eases branch target critical paths significantly but reduces performance
+  // by ~3% (based on Coremark/MHz score).
+  // Set by default in the max PMP config which has the tightest budget for branch target timing.
+  localparam bit          SpecBranch        = PMPEnable & (PMPNumRegions == 16);
 
   // IF/ID signals
   logic        dummy_instr_id;
@@ -455,6 +460,7 @@ module ibex_core #(
       .RV32B           ( RV32B           ),
       .BranchTargetALU ( BranchTargetALU ),
       .DataIndTiming   ( DataIndTiming   ),
+      .SpecBranch      ( SpecBranch      ),
       .WritebackStage  ( WritebackStage  )
   ) id_stage_i (
       .clk_i                        ( clk                      ),
